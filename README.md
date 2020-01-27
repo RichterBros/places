de4e53e3a9f5d2c8697b51f1f57eac7b7d7331b4
Alex Skreen, elliot jordan and erich richter <elliot@>
2020-01-27 14:25:55 -0800

    initial commit

--- README.md
+++ README.md
@@ -16,6 +16,16 @@ _{This is a detailed description of your application. Its purpose and usage.  Gi
 * _easy-to-understand_
 * _format_
 
+## Specifications 
+
+|   Behavior    |   Input     |     Output    |
+|--------------:|:-----------:|--------------:|
+| Create constructor. | {Location: ____,  Landmarks: ___, Notes: ____, } | {Location: Easter Island, Landmarks: Giant Heads, Notes: Had a wonderful time.} |
+| Create holder array for all locations. | function locations () { ... } | [ ] |
+| Create function to add new location to array. | locations.prototype.addLocation = function() { ... } | Location and details with unique Id } |
+
+
+
 _{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
 
 ## Known Bugs
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,91 @@
-$(document).ready(function(){
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
 
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+  
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.updateContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i].phoneNumber = "310-353- 2322";
+      }
+    }
+  };
+  return false;
+}
+
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+var AddressBook = new AddressBook();
+var contact = new Contact("Ada", "Lovelace", "503-555-0100");
+var contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+var contact3 = new Contact("Johnny", "Appleseed", "503-333-3332")
+AddressBook.addContact(contact);
+AddressBook.addContact(contact2);
+AddressBook.addContact(contact3);
+
+var look = AddressBook.findContact(2);
+AddressBook.deleteContact(3);
+var check = AddressBook.updateContact(1);
+
+$(document).ready(function () {
+  $("#contact1").click(function () {
+    $(".location1Details").fadeToggle(": " + contact.lastName + contact.phoneNumber);
+  })
+  $("#contact2").click(function () {
+    $(".location2Details").fadeToggle(": " + contact.lastName + contact.phoneNumber);
+  })
+  $("#contact3").click(function () {
+    $(".location3Details").fadeToggle(": " + contact.lastName + contact.phoneNumber);
+  })
+  
+  $(".location1Details").text(": " + contact.lastName + contact.phoneNumber);
+  $(".location2Details").text(": " + contact.lastName + contact.phoneNumber);
+  $(".location3Details").text(": " + contact.lastName + contact.phoneNumber);
+  $("#contact1").text(contact.firstName)
+  $("#contact2").text(contact2.firstName)
+  $("#contact3").text(contact3.firstName)
 });
